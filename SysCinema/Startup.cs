c7d9824148b371ce961daefa5c6238f09c6ad977
Jose Urbina <joseantonioug-09@hotmail.com>
2018-02-10 12:38:02 -0600

    Agregar archivos de proyecto.

---
+++ SysCinema/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SysCinema.Startup))]
+namespace SysCinema
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
